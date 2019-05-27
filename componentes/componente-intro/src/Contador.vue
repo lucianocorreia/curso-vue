470f37e4a671bbf8a560309db83cfdbd4df4135d
Luciano Correia <luciano.correia@gmail.com>
2019-05-27 12:41:25 -0300

    componentes desafio

--- componentes/componente-intro/src/Contador.vue
+++ componentes/componente-intro/src/Contador.vue
@@ -24,8 +24,23 @@ export default {
 };
 </script>
 
-<style>
-.contador span {
+<style scoped>
+span {
+  border-bottom: 1px solid #ccc;
+  height: 30px;
+  padding: 5px 25px;
+}
+
+button {
+  height: 30px;
+  width: 30px;
+  border-radius: 15px;
+  background-color: coral;
+  color: #fff;
+  margin-left: 10px;
+  outline: none;
+}
+/* .contador span {
   border-bottom: 1px solid #ccc;
   height: 30px;
   padding: 5px 25px;
@@ -39,5 +54,5 @@ export default {
   color: #fff;
   margin-left: 10px;
   outline: none;
-}
+} */
 </style>
--- componentes/componente-intro/src/Contadores.vue
+++ componentes/componente-intro/src/Contadores.vue
@@ -1,6 +1,7 @@
 <template>
   <div class="contadores">
-    <app-contador/>
+    <h1>Contadores</h1>
+    <app-contador v-for="c in 5" :key="c"/>
   </div>
 </template>
 
