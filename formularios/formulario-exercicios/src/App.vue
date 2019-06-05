80529ad6ad37dd9f8f66f0b02851e5d8496934a6
Luciano Correia <luciano.correia@gmail.com>
2019-06-05 11:46:15 -0300

    formulario

--- formularios/formulario-exercicios/src/App.vue
+++ formularios/formulario-exercicios/src/App.vue
@@ -2,7 +2,7 @@
   <div id="app">
     <h1>Registrar Reclamação</h1>
     <div class="conteudo">
-      <form class="painel">
+      <form class="painel" v-if="!enviado">
         <div class="cabecalho">Formulário</div>
         <Rotulo nome="E-mail">
           <input type="text" v-model.lazy.trim="usuario.email">
@@ -46,12 +46,12 @@
           </select>
         </Rotulo>
         <Rotulo nome="Primeira Reclamação?">
-          <Escolha/>
+          <Escolha v-model="escolha"/>
         </Rotulo>
         <hr>
-        <button>Enviar</button>
+        <button @click.prevent="enviar">Enviar</button>
       </form>
-      <div class="painel">
+      <div class="painel" v-else>
         <div class="cabecalho">Resultado</div>
         <Rotulo nome="E-mail">
           <span>{{ usuario.email }}</span>
@@ -79,7 +79,7 @@
           <span>{{ prioridade }}</span>
         </Rotulo>
         <Rotulo nome="Primeira Reclamação?">
-          <span>???</span>
+          <span>{{ escolha }}</span>
         </Rotulo>
       </div>
     </div>
@@ -108,12 +108,19 @@ export default {
         email: "",
         senha: "",
         idade: 25
-      }
+      },
+      escolha: true,
+      enviado: false
     };
   },
   computed: {
     tipoIdade() {
       return typeof this.usuario.idade;
+    }
+  },
+  methods: {
+    enviar() {
+      this.enviado = true;
     }
   }
 };
