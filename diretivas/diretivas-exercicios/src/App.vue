98a10bb299a77671514555946b5d6924f9ffebff
Luciano Correia <luciano.correia@gmail.com>
2019-06-05 22:36:50 -0300

    diretivas final

--- diretivas/diretivas-exercicios/src/App.vue
+++ diretivas/diretivas-exercicios/src/App.vue
@@ -8,8 +8,12 @@
     <p v-destaque:fundo.atrasar="'lightblue'">Usando diretiva personalizada</p>
     <p v-destaque="cor">Usando diretiva personalizada</p>
     <hr>
-    <p v-destaque-local:fundo.atrasar="'lightblue'">Usando diretiva personalizada</p>
-    <p v-destaque-local.atrasar.alternar="cor">Usando diretiva personalizada</p>
+    <p
+      v-destaque-local:fundo.atrasar.alternar="{cor1: 'green', cor2: 'red', atraso: 2000, intervalo: 200}"
+    >Usando diretiva personalizada</p>
+    <p
+      v-destaque-local.atrasar.alternar="{cor1: 'green', atraso: 5000}"
+    >Usando diretiva personalizada</p>
   </div>
 </template>
 
@@ -27,10 +31,10 @@ export default {
         };
 
         let atraso = 0;
-        if (binding.modifiers["atrasar"]) atraso = 3000;
+        if (binding.modifiers["atrasar"]) atraso = binding.value.atraso;
 
-        const cor1 = binding.value;
-        const cor2 = "purple";
+        const cor1 = binding.value.cor1;
+        const cor2 = binding.value.cor2;
         let corAtual = cor1;
 
         setTimeout(() => {
@@ -38,9 +42,9 @@ export default {
             setInterval(() => {
               corAtual = corAtual === cor1 ? cor2 : cor1;
               aplicarCor(corAtual);
-            }, 1000);
+            }, binding.value.intervalo);
           } else {
-            aplicarCor(binding.value);
+            aplicarCor(binding.value.cor1);
           }
         }, atraso);
       }
