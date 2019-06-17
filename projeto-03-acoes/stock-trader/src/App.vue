2c1574daee2bf52822a3a2299fba42a684f1e3b8
Luciano Correia <luciano.correia@gmail.com>
2019-06-17 13:11:59 -0300

    projeto 03 store mutations

--- projeto-03-acoes/stock-trader/src/App.vue
+++ projeto-03-acoes/stock-trader/src/App.vue
@@ -1,14 +1,25 @@
 <template>
-	<h1>Stock Trader</h1>
+    <v-app>
+        <Header/>
+        <v-content>
+            <v-container>
+                <router-view></router-view>
+            </v-container>
+        </v-content>
+    </v-app>
 </template>
 
 <script>
-export default {
+import Header from './components/Header'
 
+export default {
+    components: { Header },
+    created() {
+        this.$store.dispatch('initStocks')
+    }
 }
 </script>
 
 <style>
-
 </style>
 
